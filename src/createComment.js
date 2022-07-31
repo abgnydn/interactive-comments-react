210c563f5794f61599ba1d53a2df2762fcee1418
Ahmet Barış Günaydın <45149948+Greeeny@users.noreply.github.com>
2022-07-31 23:24:24 +0300

    axios post method configured

--- src/createComment.js
+++ src/createComment.js
@@ -1,35 +1,52 @@
-import {useState} from "react";
-export default function CommentForm({currentUser, currentUserId}){
-    const [body,setBody] = useState('');
-    const score = 0;
-    const id = currentUserId;
-    const content = body;
-    const createdAt = "";
-    const replies = [];
-    const user = currentUser;
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const newComment = { id: id,  content: content, createdAt: createdAt, score:score, user: user, replies: replies };
-    
-        fetch('http://localhost:3001/comments', {
-          method: 'POST',
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(newComment)})
-          .then(() => {
-          console.log('new comment added');
-        })
-      }
-    
-    return(
-        <div className="grid place-items-center">
-        <div className="flex flex-row max-w-2xl p-6 bg-white rounded-xl">
-            <form onSubmit= {handleSubmit} className="grid grid-cols-12 gap-2">
-                <img src={user.image.png} className="rounded-full w-4 h-4 col-span-1" alt=""></img>
-                <textarea required value={body} onChange={(e) => setBody(e.target.value)} placeholder="Add a comment..." cols="50" rows="4" className="col-span-8 border-slate-300 border rounded-sm p-3 resize-none"></textarea>
-                <button className="text-white bg-purple-500 font-bold rounded-lg mx-4 h-2/5 col-span-3">Send</button>
-            </form>
-        </div>
-        </div>
-    );
+import { useState } from "react";
+import axios from 'axios';
+
+export default function CommentForm({ currentUser, currentUserId }) {
+  const [comment, setComment] = useState("");
+  const API_URL = 'http://localhost:3000/comments';
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newComment = {
+      id: currentUserId,
+      content: comment,
+      createdAt: "",
+      score: 0,
+      user: currentUser,
+      replies: [],
+    };
+
+    axios
+      .post(API_URL,  newComment )
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+      });
+  };
+
+  return (
+    <div className="grid place-items-center">
+      <div className="flex flex-row max-w-2xl p-6 bg-white rounded-xl">
+        <form onSubmit={handleSubmit} className="grid grid-cols-12 gap-2">
+          <img
+            src={currentUser.image.png}
+            className="rounded-full w-4 h-4 col-span-1"
+            alt=""
+          ></img>
+          <textarea
+            required
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            placeholder="Add a comment..."
+            cols="50"
+            rows="4"
+            className="col-span-8 border-slate-300 border rounded-sm p-3 resize-none"
+          ></textarea>
+          <button className="text-white bg-purple-500 font-bold rounded-lg mx-4 h-2/5 col-span-3">
+            Send
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
