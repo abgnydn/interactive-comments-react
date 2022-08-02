b1f296c2627750a0967e3a44b28e0e4dae43098b
Ahmet Barış Günaydın <45149948+Greeeny@users.noreply.github.com>
2022-08-02 19:35:38 +0300

    edit button created

---
+++ src/editButton.js
@@ -0,0 +1,13 @@
+import { MdOutlineModeEditOutline } from "react-icons/md";
+
+export default function EditButton ({handleEdit}){
+    return (
+        <>
+        <div className="text-purple-500 font-bold ml-4">
+            <button className="flex flex-row flex-nowrap" onClick={handleEdit}><MdOutlineModeEditOutline className="mr-1 mb-1 inline-block mt-1"/><div className="inline-block">Edit</div></button>            
+        </div>
+        <div>
+        </div>
+        </>
+    )
+}
