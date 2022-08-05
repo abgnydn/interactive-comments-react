c941a1837a476cf0826ce4ff1550b5d5b818de58
Ahmet Barış Günaydın <45149948+Greeeny@users.noreply.github.com>
2022-08-05 19:56:10 +0300

    Nothing Change

--- src/createComment.js
+++ src/createComment.js
@@ -26,12 +26,13 @@ export default function CommentForm({ currentUser, comments }) {
   return (
     <div className="grid place-items-center">
       <div className="flex flex-row lg:w-3/5 md:w-2/3 sm:w-full p-6 bg-white rounded-xl">
-        <form onSubmit={handleSubmit} className="grid grid-cols-12 gap-2 w-full">
-          <img
+      <img
             src={currentUser.image.png}
-            className="rounded-full w-10 h-10 col-span-1"
+            className="rounded-full w-10 h-10 col-span-1 mr-4"
             alt=""
           ></img>
+        <form onSubmit={handleSubmit} className="grid grid-cols-12 gap-2 w-full">
+          
           <textarea
             required
             value={comment}
@@ -39,10 +40,10 @@ export default function CommentForm({ currentUser, comments }) {
             placeholder="Add a comment..."
             cols="50"
             rows="4"
-            className="col-span-8 border-slate-300 border rounded-sm p-3 resize-none"
+            className="col-span-9 border-slate-300 border rounded-sm p-3 resize-none"
           ></textarea>
-          <button className="text-white bg-purple-500 font-bold rounded-lg mx-4 h-2/5 col-span-3">
-            Send
+          <button className="text-white bg-purple-500 font-bold rounded-lg ml-2 h-2/5 col-span-3">
+            SEND
           </button>
         </form>
       </div>
