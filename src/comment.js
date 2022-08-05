1d79a96048c72f71863c1ebb254a233fca7e019c
Ahmet Barış Günaydın <45149948+Greeeny@users.noreply.github.com>
2022-08-05 19:53:27 +0300

    Comment component created and moved from parent

---
+++ src/comment.js
@@ -0,0 +1,108 @@
+import React from "react";
+import ContentSection from "./contentSection";
+import ScoreButton from "./buttons/scoreButton";
+import ReplyButton from "./buttons/replyButton";
+import DeleteButton from "./buttons/deleteButton";
+import EditButton from "./buttons/editButton";
+import EditSection from "./editSection";
+import Replies from "./replies";
+import ReplySection from "./replySection";
+import { useState } from "react";
+
+const Comment = ({ comment, currentUser }) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [editSectionVisibility, setEditSectionVisibility] = useState(false);
+  const [replySectionVisibility, setReplySectionVisibility] = useState(false);
+
+  const handleEdit = (e) => {
+    e.preventDefault();
+    setEditSectionVisibility(!editSectionVisibility);
+    setReplySectionVisibility(replySectionVisibility);
+
+    //TODO: Control edit section and reply section visibility with the id.
+  };
+  const handleReply = (e, key) => {
+    e.preventDefault();
+    setEditSectionVisibility(editSectionVisibility);
+    setReplySectionVisibility(!replySectionVisibility);
+  };
+
+  return (
+    <>
+      <div className="flex rounded-xl p-6 bg-white my-3 flex-initial w-full">
+        <ScoreButton score={comment.score} />
+        <div className="flex flex-col w-full">
+          <div className="flex flex-row place-content-between">
+            <div className="flex flex-row">
+              <img
+                src="./images/avatars/image-amyrobson.png"
+                className="w-10 h-10 rounded-full mr-3"
+                alt=""
+              ></img>
+              <p className="font-bold mr-3">{comment.user.username}</p>
+              {comment.user.username === currentUser.username && (
+                <div className="text-white bg-purple-500 px-2  rounded-sm h-2/3 mr-2 ">
+                  you
+                </div>
+              )}
+              <div>
+                <p className="text-slate-400 break-all font-semibold">
+                  {comment.createdAt}
+                </p>
+              </div>
+            </div>
+            <div>
+              {comment.user.username === currentUser.username ? (
+                <div className="flex flex-row">
+                  <DeleteButton
+                    handleOpen={handleOpen}
+                    id={comment.id}
+                    open={open}
+                    handleClose={handleClose}
+                  />
+                  <EditButton handleEdit={handleEdit} value={comment.id} />
+                </div>
+              ) : (
+                <ReplyButton
+                  reply={null}
+                  comment={comment.id}
+                  handleReply={handleReply}
+                />
+              )}
+            </div>
+          </div>
+
+          {editSectionVisibility ? (
+            <EditSection comment={comment} visibility={editSectionVisibility} />
+          ) : (
+            <ContentSection comment={comment} />
+          )}
+        </div>
+      </div>
+          {comment.reply !== null && (
+            <div className="w-fit">
+              <Replies
+                replies={comment.replies}
+                currentUser={currentUser}
+                handleOpen={handleOpen}
+                handleReply={handleReply}
+                comment={comment}
+              />
+            </div>
+          )}
+      <div>
+        <ReplySection
+          id={comment.id}
+          comment={comment}
+          currentUser={currentUser}
+          visibility={replySectionVisibility}
+          replySection={false}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Comment;
--- src/commentsList.js
+++ src/commentsList.js
@@ -1,95 +1,18 @@
-import Replies from "./replies";
-import ScoreButton from "./scoreButton";
-import ReplyButton from "./replyButton";
-import DeleteButton from "./deleteButton";
-import EditButton from "./editButton";
-import ReplySection from "./replySection";
-import { useState } from "react";
-import EditSection from "./editSection";
-export default function CommentsList({ comments, currentUser, handleOpen }) {
-  const [replySection, setReplySection] = useState(false);
 
-  const handleReply = (e) => {
-    e.preventDefault();
-    setReplySection(!replySection);
-    setEditSection(editSection);
-  };
-  const [editSection, setEditSection] = useState(false);
+import Comment from "./comment";
 
-  const handleEdit = (e) => {
-    e.preventDefault();
-    setEditSection(!editSection);
-    setReplySection(replySection);
-  };
+export default function CommentsList({ comments, currentUser}) {
+
   return (
-    <div className="grid place-items-center my-20">
-      <div>
-      <div className="flex lg:w-3/5 flex-col md:w-2/3 sm:w-full shrink-0">
-        {comments.map((comment) => (
-          <div key={comment.id}>
-            <div className="flex rounded-xl p-6 bg-white my-3 flex-initial w-full">
-              <ScoreButton score={comment.score} />
-              <div className="flex flex-col w-full">
-                <div className="flex flex-row place-content-between">
-                  <div className="flex flex-row">
-                    <img
-                      src="./images/avatars/image-amyrobson.png"
-                      className="w-10 h-10 rounded-full mr-3"
-                      alt=""
-                    ></img>
-                    <p className="font-bold mr-3">{comment.user.username}</p>
-                    {comment.user.username === currentUser.username && (
-                      <div className="text-white bg-purple-500 px-2  rounded-sm h-2/3 mr-2 ">
-                        you
-                      </div>
-                    )}
-                    <div>
-                      <p className=" text-slate-400 break-all">
-                        {comment.createdAt}
-                      </p>
-                    </div>
-                  </div>
-                  <div>
-                    {comment.user.username === currentUser.username ? (
-                      <div className="flex flex-row">
-                        <DeleteButton handleOpen={handleOpen} id={comment.id} />
-                        <EditButton handleEdit={handleEdit} />
-                      </div>
-                    ) : (
-                      <ReplyButton handleReply={handleReply} />
-                    )}
-                  </div>
-                </div>
-
-                <p className="text-slate-600 break-all">{comment.content}</p>
-              </div>
-              
+    <div>
+      <div className="grid place-items-center">
+        <div className="flex lg:w-3/5 flex-col md:w-2/3 sm:w-full shrink-0">
+          {comments.map((comment) => (
+            <div key={comment.id}>
+             <Comment  comment={comment} currentUser={currentUser}/>
             </div>
-
-            {comment.reply !== null && (
-              <div className="w-fit">
-                <Replies
-                  replies={comment.replies}
-                  handleReply={handleReply}
-                  currentUser={currentUser}
-                  handleOpen={handleOpen}
-                />
-              </div>
-            )}
-            <div>
-                <EditSection
-                  visibility={editSection}
-                  currentUser={currentUser}
-                />
-                <ReplySection
-                  visibility={replySection}
-                  currentUser={currentUser}
-                  id={comment.id}
-                />
-              </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
       </div>
     </div>
   );
